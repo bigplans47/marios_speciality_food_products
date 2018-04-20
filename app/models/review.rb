90c29431dfd6e19adbdacd5498667353cc906236
Dan Kiss <flowfast47@gmail.com>
2018-04-20 16:01:26 -0700

    add validation of rating and passing spec on it

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,9 @@
 class Review < ActiveRecord::Base
   belongs_to :product
   validates :author, :content_body, :rating, :presence => true
+  validates :rating, numericality: { only_integer: true} #railsdocs
+  validates_numericality_of :rating, less_than_or_equal_to: 5 #shouldamatchers docs
+  validates_numericality_of :rating, greater_than_or_equal_to: 1
 end
+
+# greater_than_or_equal_to: 1, less_then_or_equal_to: 5
