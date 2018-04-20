8cbab1b22b05ee9365be54bd9043fd28ad3beec8
Dan Kiss <flowfast47@gmail.com>
2018-04-20 16:22:21 -0700

    add validation and passing spec for content_body of review

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,6 +4,8 @@ class Review < ActiveRecord::Base
   validates :rating, numericality: { only_integer: true} #railsdocs
   validates_numericality_of :rating, less_than_or_equal_to: 5 #shouldamatchers docs
   validates_numericality_of :rating, greater_than_or_equal_to: 1
+  validates_length_of :content_body, minimum: 50, on: :create
+  validates_length_of :content_body, maximum: 250, on: :create
 end
 
 # greater_than_or_equal_to: 1, less_then_or_equal_to: 5
