5d01289fbf6be0f77763f5d962388a2245c600c2
Dan Kiss <flowfast47@gmail.com>
2018-04-20 17:23:08 -0700

    add scope for 3 most recently added products, also all products made in usa scope

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,17 @@
 class Product < ActiveRecord::Base
   validates :name, :cost, :country_of_origin, :presence => true
   has_many :reviews
-  scope :most_reviews, -> {
+  scope :most_reviewed_product, -> {
     select("products.id, products.name, count(reviews.id) as reviews_count")
     .joins(:reviews)
     .group("products.id")
     .order("reviews_count DESC")
     .limit(1)
+  }
+  scope :three_most_recent_added, -> {
+    order(:created_at => :desc).limit(3)
+  }
+  scope :find_usa_product, -> (country_parameter) {
+    where(:country_of_origin => country_parameter)
   }
 end
