c2cf985af6d08fab2d132a6102b7dfeaea6483e9
Dan Kiss <flowfast47@gmail.com>
2018-04-20 22:31:40 -0700

    add new layout to landing page

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -2,7 +2,7 @@ class LandingController < ApplicationController
   def index
     @top_product = Product.most_reviewed_product
     @three_most_recent_products = Product.three_most_recent_added
-    @made_in_usa = Product.find_usa_product('USAA')
+    @made_in_usa = Product.find_usa_product('USA')
     p @top_product, @three_most_recent_products, @made_in_usa
   end
 
