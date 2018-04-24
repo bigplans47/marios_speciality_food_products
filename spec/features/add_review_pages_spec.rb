bc97aeb6fecab8a6040daeb7579d0b3a3bbf1f8d
Dan Kiss <Guest@Epicodus-4E.local>
2018-04-24 10:53:52 -0700

    update integration spec to pass with authentication

--- spec/features/add_review_pages_spec.rb
+++ spec/features/add_review_pages_spec.rb
@@ -3,14 +3,19 @@ require 'rails_helper'
 describe "the add a review process, update review and remove review" do
   it 'add a review' do
     @product = Product.create(:name => 'Apple',:cost => 2, :country_of_origin => 'USA')
+    visit signup_path
+    fill_in 'user_email', :with => 'bill@gmail.com'
+    fill_in 'user_password', :with => 'billiscool'
+    fill_in 'user_password_confirmation', :with => 'billiscool'
+    click_on 'Sign up now'
+    expect(page).to have_content 'You\'ve successfully signed up'
     visit new_product_review_path(@product)
-    fill_in 'Author', :with => 'Bill Smith'
+    fill_in 'review_author', :with => 'Bill Smith'
     fill_in 'Content body', :with => "So good, An apple is a sweet, edible fruit produced by an apple tree (Malus pumila). Apple trees are cultivated worldwide as a fruit tree, and is the most widely grown "
     fill_in 'Rating', :with => 5
     click_on 'Create Review'
     expect(page).to have_content 'So good, An apple is a sweet, edible fruit produced by an apple tree (Malus pumila). Apple trees are cultivated worldwide as a fruit tree, and is the most widely grown '
   end
-  # :author, :content_body, :rating
 
   it 'updates review' do
     @product = Product.create(:name => 'Apple',:cost => 2, :country_of_origin => 'USA')
